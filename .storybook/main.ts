2e016d9c9936a283a79228ad82f73bba04869e51
unional <homawong@gmail.com>
2025-11-27 21:51:04 -0800

    chore: init

---
+++ .storybook/main.ts
@@ -0,0 +1,15 @@
+import { defineMain } from '@storybook/react-vite/node'
+
+export default defineMain({
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs"
+  ],
+  "framework": "@storybook/react-vite"
+})
